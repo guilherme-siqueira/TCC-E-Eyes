b059bf0a3745ebe5239429112977cf501b86cadd
guilherme-siqueira <gsiqueira@daitangroup.com>
2017-02-02 23:29:34 -0200

    Update ApiKeys.java

--- E-Eyes/app/src/main/java/br/usp/guilherme_galdino_siqueira/e_eyes/properties/ApiKeys.java
+++ E-Eyes/app/src/main/java/br/usp/guilherme_galdino_siqueira/e_eyes/properties/ApiKeys.java
@@ -5,7 +5,7 @@ package br.usp.guilherme_galdino_siqueira.e_eyes.properties;
  */
 
 public class ApiKeys {
-    public static final String YANDEX_API_KEY = "trnsl.1.1.20160709T203633Z.f5fc2f765a9c9a46.bb3ca34c1b1fdff081f830004f56534782b8c0bd";
+    public static final String YANDEX_API_KEY = "xxx";
 
-    public static final String CLOUD_VISION_KEY = "AIzaSyAVLpAy8Pd-ZZ9gMRSbcpFwTCt22KEor3o";
+    public static final String CLOUD_VISION_KEY = "xxx";
 }
